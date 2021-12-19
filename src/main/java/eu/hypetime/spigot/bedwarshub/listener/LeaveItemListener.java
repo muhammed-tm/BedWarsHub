bf8e6d43eb7756d5d3ef923e8110c0b0f0e7537c
BuildTools <unconfigured@null.spigotmc.org>
2021-12-19 02:02:24 +0100

    replace kick with send on leave

--- src/main/java/eu/hypetime/spigot/bedwarshub/listener/LeaveItemListener.java
+++ src/main/java/eu/hypetime/spigot/bedwarshub/listener/LeaveItemListener.java
@@ -1,20 +1,31 @@
 package eu.hypetime.spigot.bedwarshub.listener;
 
-import eu.hypetime.spigot.bedwarshub.utils.Inventories;
+import de.dytanic.cloudnet.driver.CloudNetDriver;
+import de.dytanic.cloudnet.ext.bridge.player.IPlayerManager;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class LeaveItemListener implements Listener {
 
+    @EventHandler
     public void onInteract(PlayerInteractEvent event) {
-        if(event.getItem() == null) return;
-        if(event.getItem().getType() == Material.AIR) return;
-        if(event.getItem().getType() == Material.PLAYER_HEAD) {
-            if(!event.getItem().hasItemMeta()) return;
-            if(!event.getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§8» §cVerlassen")) {
-                Inventories.mainInventory(event.getPlayer());
+        if (event.getItem() == null) return;
+        if (event.getItem().getType() == Material.AIR) return;
+        if (event.getItem().getType() == Material.PLAYER_HEAD) {
+            if (!event.getItem().hasItemMeta()) return;
+            if (event.getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§8» §cVerlassen")) {
+                sendPlayer(event.getPlayer(), "Lobby-1");
             }
         }
     }
+
+    public void sendPlayer(Player player, String server) {
+        CloudNetDriver.getInstance().getServicesRegistry().getFirstService(IPlayerManager.class)
+                .getPlayerExecutor(player.getUniqueId())
+                .connect(server);
+    }
+
 }
